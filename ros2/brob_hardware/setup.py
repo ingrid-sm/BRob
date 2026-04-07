2b9d317fd5c770fde09d358f37f1ad79df9bcd85
ingrid-sm <ingrid.sg.lm@gmail.com>
2026-04-07 18:37:54 +0200

    Update setup.py

--- ros2/brob_hardware/setup.py
+++ ros2/brob_hardware/setup.py
@@ -1,28 +1,31 @@
 from setuptools import find_packages, setup
 
-
-package_name = "brob_hardware"
-
+package_name = 'brob_hardware'
 
 setup(
     name=package_name,
-    version="0.0.0",
-    packages=find_packages(exclude=["test"]),
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
-        ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
-        (f"share/{package_name}", ["package.xml"]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=["setuptools"],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer="Ingrid",
-    maintainer_email="your_email@example.com",
-    description="ROS 2 bridge nodes for the BRob robot hardware and vision API.",
-    license="MIT",
-    tests_require=["pytest"],
+    maintainer='ingrid',
+    maintainer_email='ingrid@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
-        "console_scripts": [
-            "command_bridge_node = brob_hardware.command_bridge_node:main",
-            "vision_bridge_node = brob_hardware.vision_bridge_node:main",
+        'console_scripts': [
+     		'command_bridge = brob_hardware.command_bridge_node:main',
+     		'vision_bridge = brob_hardware.vision_bridge_node:main',
         ],
     },
 )
